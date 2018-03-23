e9e7b407ae4fed5e1730be62d6cb8ea5019ffecc
Reid South <rbsouth@gmail.com>
2018-03-22 20:03:35 -0600

    complete, wrong version

--- eatin-rails/config/routes.rb
+++ eatin-rails/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	#   post 'user_token' => 'user_token#create'
 	# end
 	resources :users
+	post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
